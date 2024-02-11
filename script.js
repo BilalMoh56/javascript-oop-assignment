26e1a919bb7093ec8621f699586de264805ef5a5
BilalMoh56 <149912780+BilalMoh56@users.noreply.github.com>
2024-02-11 18:10:16 -0500

    1

--- script.js
+++ script.js
@@ -7,3 +7,36 @@
 // (This is called a 'getter' in Object Oriented Programming)
 
 // If you're stuck, take a look at the example class here for reference: https://www.geeksforgeeks.org/introduction-object-oriented-programming-javascript/#
+
+// Step 1: Define Car Class
+class Car{
+    constructor(make, model, year ,color){
+        this.make= make;
+        this.model= model;
+        this.year= year;
+        this.color= color;
+    }
+//Step 2: Create getter methods:
+    getMake() { // retrieves make property
+        return this.make;
+    }
+    getModel() {  // retrieves model property
+        return this.model;
+    }
+    getYear() {  // retrieves year property
+        return this.year;
+    }
+    getColor() {  // retrieves color property
+        return this.color;
+    }
+}
+
+
+// Usage of the Car Class
+const myCar = new Car("Nissan", "Altima 2.5sl",2011, "Black")
+
+// Accessing properties using the getter methods
+console.log("Make:", myCar.getMake());
+console.log("Model:", myCar.getModel());
+console.log("Year:", myCar.getYear());
+console.log("Color:", myCar.getColor());
